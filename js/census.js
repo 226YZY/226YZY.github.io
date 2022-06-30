a3cf0b9a517b0e753d40a3cc1a3db43d811fbf6f
226YZY <2119388133@qq.com>
2022-06-30 19:52:24 +0800

    Site updated: 2022-06-30 19:52:09

--- js/census.js
+++ js/census.js
@@ -1,39 +1,41 @@
 //https://baidu-tongji-api-sigma.vercel.app/api?access_token=121.2cd650462e0eab5f81438cc8a69ff0b4.YG5bpDwMakUmioVVTsxKNq1Tj1it9Gy-Qj7z8en.cNunNA&site_id=16940767&start_date=20210705&end_date=20220127&metrics=pv_count&method=visit/district/a
 var start_date = '20210705' // 开始日期
-var date = new Date();
-var end_date = '' + date.getFullYear() + (date.getMonth() > 8 ? (date.getMonth() + 1) : ("0" + (date.getMonth() + 1))) + (date.getDate() > 9 ? date.getDate() : ("0" + date.getDate())); // 结束日期
-var access_token = '121.2cd650462e0eab5f81438cc8a69ff0b4.YG5bpDwMakUmioVVTsxKNq1Tj1it9Gy-Qj7z8en.cNunNA' // accessToken
+var date = new Date() //;
+var end_date ='' + date.getFullYear() + (date.getMonth() > 8 ? (date.getMonth() + 1) : ("0" + (date.getMonth() + 1))) + (date.getDate() > 9 ? date.getDate() : ("0" + date.getDate())); // 结束日期
+var access_token = '121.6c22f87a35cc883c75adc9290476a9ea.YC1qqlWIrbgpK95Wj1hmWaziW38KxuMtY9oN_cn.lx5yzw' // accessToken
 var site_id = '16940767' // 网址id
 var dataUrl = 'https://baidu-tongji-api-sigma.vercel.app/api?access_token=' + access_token + '&site_id=' + site_id
 var metrics = 'pv_count' // 统计访问次数 PV 填写 'pv_count'，统计访客数 UV 填写 'visitor_count'，二选一
 var metricsName = (metrics === 'pv_count' ? '访问次数' : (metrics === 'visitor_count' ? '访客数' : ''))
 // 这里为了统一颜色选取的是“明暗模式”下的两种字体颜色，也可以自己定义
 var color = document.documentElement.getAttribute('data-theme') === 'light' ? '#4c4948' : 'rgba(255,255,255,0.7)'
-var myvisit = 2681 + 5600; //我本人大概访问的次数，用于修正浙江省的访问数据
+var myvisit = 2681 + 5700; //我本人大概访问的次数，用于修正浙江省的访问数据
 var othervisit=1180;
 // 访问地图
 function mapChart() {
   let script = document.createElement("script")
-  let paramUrl = '&start_date=' + start_date + '&end_date=' + end_date + '&metrics=' + metrics + '&method=visit/district/a';
+  //let paramUrl = '&start_date=' + start_date + '&end_date=' + end_date + '&metrics=' + metrics + '&method=visit/district/a';
+
+  let paramUrl = '&start_date=' + start_date + '&end_date=' + end_date + '&metrics=' + metrics + '&method=overview/getDistrictRpt';
   fetch(dataUrl + paramUrl).then(data => data.json()).then(data => {
     let mapName = data.result.items[0]
     let mapValue = data.result.items[1]
     let mapArr = []
     let max = mapValue[0][0] - myvisit
     for (let i = 0; i < mapName.length; i++) {
-      if (mapName[i][0].name == "浙江") {
+      if (mapName[i][0] == "浙江") {
         mapArr.push({
-          name: mapName[i][0].name,
+          name: mapName[i][0],
           value: (mapValue[i][0] - myvisit)
         })
-      }else if (mapName[i][0].name == "河南") {
+      }else if (mapName[i][0]== "河南") {
         mapArr.push({
-          name: mapName[i][0].name,
+          name: mapName[i][0],
           value: (mapValue[i][0] - othervisit)
         })
       } else {
         mapArr.push({
-          name: mapName[i][0].name,
+          name: mapName[i][0],
           value: mapValue[i][0]
         })
       }
@@ -117,6 +119,8 @@ function trendsChart() {
       }
       let monthArrJson = JSON.stringify(monthArr)
       let monthValueArrJson = JSON.stringify(monthValueArr)
+
+      
       script.innerHTML = `
         var trendsChart = echarts.init(document.getElementById('trends-chart'), 'light');
         var trendsOption = {
@@ -254,6 +258,9 @@ function sourcesChart() {
         }
       }
       let sourcesArrJson = JSON.stringify(sourcesArr)
+
+      console.log(metricsName);
+      console.log(sourcesArrJson);
       script.innerHTML = `
         var sourcesChart = echarts.init(document.getElementById('sources-chart'), 'light');
         var sourcesOption = {
